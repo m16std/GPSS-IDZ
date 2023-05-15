779fe79e0d6cccddef78778d0f5af9b410711ac4
m16std <uwm16@yandex.ru>
2023-05-16 01:22:42 +0300

    IT WORKS

--- GPSS-IDZ/MainWindow.xaml.cs
+++ GPSS-IDZ/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AngouriMath;
+using AngouriMath.Extensions;
+using static AngouriMath.MathS;
 
 namespace GPSS_IDZ
 {
@@ -25,15 +28,224 @@ namespace GPSS_IDZ
             InitializeComponent();
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        int function_num = 0;
+        int new_function = 0;
+        Methods methods = new Methods();
+
+        private void Calculation(object sender, RoutedEventArgs e)
         {
+            double Mo1, Mo2, a, S1, bd, ce, Mo1DivMo2, PointsCount;
+
+            try
+            {
+                bd = (double)start.Text.EvalNumerical();
+                ce = (double)end.Text.EvalNumerical();
+                Mo1DivMo2 = (double)mo1divmo2.Text.EvalNumerical();
+                PointsCount = (double)pointscount.Text.EvalNumerical();
+
+                List<double> D = new List<double>() { (double)d0.Text.EvalNumerical(), (double)d1.Text.EvalNumerical(), (double)d2.Text.EvalNumerical(), (double)d3.Text.EvalNumerical() };
+                List<double> E = new List<double>() { (double)e0.Text.EvalNumerical(), (double)e1.Text.EvalNumerical(), (double)e2.Text.EvalNumerical(), (double)e3.Text.EvalNumerical() };
+                List<double> K = new List<double>() { (double)k0.Text.EvalNumerical(), (double)k1.Text.EvalNumerical(), (double)k2.Text.EvalNumerical(), (double)k3.Text.EvalNumerical() };
+                List<double> Y = new List<double>() { (double)y0.Text.EvalNumerical(), (double)y1.Text.EvalNumerical(), (double)y2.Text.EvalNumerical(), (double)y3.Text.EvalNumerical() };
+                
+                if(PointsCount > 4)
+                {
+                    D.Add((double)d4.Text.EvalNumerical());
+                    E.Add((double)e4.Text.EvalNumerical());
+                    K.Add((double)k4.Text.EvalNumerical());
+                    Y.Add((double)y4.Text.EvalNumerical());
+                }
+                if (PointsCount > 5)
+                {
+                    D.Add((double)d5.Text.EvalNumerical());
+                    E.Add((double)e5.Text.EvalNumerical());
+                    K.Add((double)k5.Text.EvalNumerical());
+                    Y.Add((double)y5.Text.EvalNumerical());
+                }
+                if (PointsCount > 6)
+                {
+                    D.Add((double)d6.Text.EvalNumerical());
+                    E.Add((double)e6.Text.EvalNumerical());
+                    K.Add((double)k6.Text.EvalNumerical());
+                    Y.Add((double)y6.Text.EvalNumerical());
+                }
+
+                List<double> A = new List<double>() { (double)a0.Text.EvalNumerical(), (double)a1.Text.EvalNumerical(), (double)a2.Text.EvalNumerical(), (double)a3.Text.EvalNumerical() };
+                List<double> B = new List<double>() { (double)b0.Text.EvalNumerical(), (double)b1.Text.EvalNumerical(), (double)b2.Text.EvalNumerical(), (double)b3.Text.EvalNumerical() };
+                List<double> P = new List<double>() { (double)p0.Text.EvalNumerical(), (double)p1.Text.EvalNumerical(), (double)p2.Text.EvalNumerical(), (double)p3.Text.EvalNumerical() };
+
+                S1 = methods.GetS1(bd, ce, D, E, K, Y);
+
+                Mo1 = methods.GetMo1(S1, bd, ce, D, E, K, Y);
+
+                Mo2 = methods.FindMo2(Mo1 / Mo1DivMo2, Mo.Text, function_num, new_function, fx.Text, bd, ce);
+
+                a = methods.GetP0(A, B, P, Mo1, Mo2);
+
+                methods.PrintP1(S1, bd, ce, D, E, K, Y);
+
+                log.Text = methods.LogOut;
+
+                Mo1TB.Text = Mo1.ToString("F4");
+                Mo2TB.Text = Mo2.ToString("F4");
+                MoDivTB.Text = (Mo1/Mo2).ToString("F1");
+                aTB.Text = a.ToString("F2");
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка");
+                return;
+            }
+
 
         }
 
-        private void Mo_set_1(int num)
+        #region "OBRABOTCHIK"
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
         {
-
+            new_function = 1;
         }
 
+        private void checkBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            new_function = 0;
+        }
+
+
+        private void tester(object sender, RoutedEventArgs e)
+        {
+            Mo.Text = "e^(a + b^2 / 2) + c";
+            function_num = 15;
+            pointscount.Text = "6";
+            start.Text = "0";
+            end.Text = "22";
+            mo1divmo2.Text = "3";
+
+            a0.Text = "1";
+            a1.Text = "1";
+            a2.Text = "7";
+            a3.Text = "8";
+            b0.Text = "0";
+            b1.Text = "1";
+            b2.Text = "0";
+            b3.Text = "0";
+            p0.Text = "0.4";
+            p1.Text = "0.42";
+            p2.Text = "0.11";
+            p3.Text = "0.07";
+
+            d0.Text = "1";
+            e0.Text = "0";
+            k0.Text = "1";
+            y0.Text = "1";
+
+            d1.Text = "4";
+            e1.Text = "1";
+            k1.Text = "5";
+            y1.Text = "1";
+
+            d2.Text = "3";
+            e2.Text = "2";
+            k2.Text = "5";
+            y2.Text = "0";
+
+            d3.Text = "2";
+            e3.Text = "3";
+            k3.Text = "5";
+            y3.Text = "0";
+
+            d4.Text = "1";
+            e4.Text = "4";
+            k4.Text = "5";
+            y4.Text = "2";
+
+            d5.Text = "0";
+            e5.Text = "1";
+            k5.Text = "1";
+            y5.Text = "2";
+        }
+
+        private void Mo_set_1(object sender, RoutedEventArgs e)
+        {
+            Mo.Text = "c * (b - a) / (c + d) + a";
+            function_num = 1;
+        }
+        private void Mo_set_2(object sender, RoutedEventArgs e)
+        {
+            Mo.Text = "a * b";
+            function_num = 2;
+        }
+        private void Mo_set_3(object sender, RoutedEventArgs e)
+        {
+            Mo.Text = "(a + b) / 2";
+            function_num = 3;
+        }
+        private void Mo_set_4(object sender, RoutedEventArgs e)
+        {
+            Mo.Text = "a + b";
+            function_num = 4;
+        }
+        private void Mo_set_7(object sender, RoutedEventArgs e)
+        {
+            Mo.Text = "a * b + c";
+            function_num = 7;
+        }
+        private void Mo_set_8(object sender, RoutedEventArgs e)
+        {
+            Mo.Text = "(1 - a) / a";
+            function_num = 8;
+        }
+        private void Mo_set_11(object sender, RoutedEventArgs e)
+        {
+            Mo.Text = "a";
+            function_num = 11;
+        }
+        private void Mo_set_12(object sender, RoutedEventArgs e)
+        {
+            Mo.Text = "a";
+            function_num = 12;
+        }
+        private void Mo_set_15(object sender, RoutedEventArgs e)
+        {
+            Mo.Text = "e^(a + b^2 / 2.0) + c";
+            function_num = 15;
+        }
+        private void Mo_set_16(object sender, RoutedEventArgs e)
+        {
+            Mo.Text = "a * (1 - b) / b";
+            function_num = 16;
+        }
+        private void Mo_set_17(object sender, RoutedEventArgs e)
+        {
+            Mo.Text = "a";
+            function_num = 17;
+        }
+        private void Mo_set_18(object sender, RoutedEventArgs e)
+        {
+            Mo.Text = "(a * b) / (a - 1)";
+            function_num = 18;
+        }
+        private void Mo_set_19(object sender, RoutedEventArgs e)
+        {
+            Mo.Text = "b / (a - 1) + c";
+            function_num = 19;
+        }
+        private void Mo_set_21(object sender, RoutedEventArgs e)
+        {
+            Mo.Text = "a";
+            function_num = 21;
+        }
+        private void Mo_set_22(object sender, RoutedEventArgs e)
+        {
+            Mo.Text = "(a + b + c) / 3";
+            function_num = 22;
+        }
+        private void Mo_set_23(object sender, RoutedEventArgs e)
+        {
+            Mo.Text = "(a + b) / 2";
+            function_num = 23;
+        }
+
+        #endregion
     }
 }
---
+++ GPSS-IDZ/Methods.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using AngouriMath;
+using AngouriMath.Extensions;
+using static System.Console;
+using static AngouriMath.MathS;
+namespace GPSS_IDZ
+{
+    internal class Methods
+    {
+        public string LogOut = "";
+        public void PrintP2(double a, double b, double c, double d, int variant, int new_function, string fx, double bd, double ce)
+        {
+            a = Math.Round(a * 100) / 100;
+            b = Math.Round(b * 100) / 100;
+            c = Math.Round(c * 100) / 100;
+            d = Math.Round(d * 100) / 100;
+
+            if (new_function == 1)
+            {
+                for (double xi = bd; xi < ce; xi += 1)
+                {
+                    LogOut += Math.Round((double)fx.Substitute("a", a).Substitute("b", b).Substitute("c", c).Substitute("d", d).Substitute("x", xi).EvalNumerical(), 3) + ", " + Math.Round(xi) + "/\n";
+                }
+            }
+
+            if (variant == 1)//A1 A1 a1 a2
+                LogOut += "BETA(1, " + a + ", " + b + ", " + c + ", " + d + ")";
+
+            if (variant == 2)//t p - -
+                LogOut += "BINORMAN(1, " + a + ", " + b + ")";
+
+            if (variant == 3)//i j - -
+                LogOut += "DUNIFORM(1, " + a + ", " + b + ")";
+
+            if (variant == 4)//B A - -
+                LogOut += "EXPONENTIAL(1, " + b + ", " + a + ")";
+
+            if (variant == 7)//a b A -
+                LogOut += "GAMMA(1, " + c + ", " + b + ", " + c + ")";
+
+            if (variant == 8)//p - - -
+                LogOut += "GEOMETRIC(1, " + a + ")";
+
+            if (variant == 11)//A - - -
+                LogOut += "LAPLACE(1, " + a + ", 1)";
+
+            if (variant == 12)//A - - -
+                LogOut += "LOGISTIC(1, " + a + ", 1)";
+
+            if (variant == 15)//u o A -
+                LogOut += "LOGNORMAL(1, " + c + ", " + a + ", " + b + ")";
+
+            if (variant == 16)//s p - -
+                LogOut += "NEGBINOM(1, " + a + ", " + b + ")";
+
+            if (variant == 17)//u - - -
+                LogOut += "NORMAL(1, " + a + ", 1)";
+
+            if (variant == 18)//b A - -
+                LogOut += "PARETO(1, " + b + ", " + a + ")";
+
+            if (variant == 19)//a b A -
+                LogOut += "PEARSON5(1, " + c + ", " + b + ", " + a + ")";
+
+            if (variant == 21)//A - - -
+                LogOut += "POISSON(1, " + a + ")";
+
+            if (variant == 22)//a b c -
+                LogOut += "TRIANGULAR(1, 10" + a + ", " + b + ", " + c + ")";
+
+            if (variant == 23)//a b - -
+                LogOut += "UNIFORM(1, " + a + ", " + b + ")";
+
+            if (variant == 24)//a b A -
+                LogOut += "WEIBULL(1, " + c + ", " + b + ", " + a + ")";
+
+            LogOut += "\n\n";
+        }
+
+        public double FindMo2(double x, string Mo, int function_num, int new_function, string fx, double bd, double ce)
+        {
+            double delta;
+            for (double d = 0; d < 100; d += 0.1)
+            {
+                for (double c = 0; c < 100; c += 0.1)
+                {
+                    for (double b = 0; b < 100; b += 0.1)
+                    {
+                        for (double a = 0; a < 100; a += 0.1)
+                        {
+                            delta = Math.Abs(x - (double)Mo.Substitute("a", a).Substitute("b", b).Substitute("c", c).Substitute("d", d).EvalNumerical());
+                            if (delta < 0.1)
+                            {
+                                PrintP2(a, b, c, d, function_num, new_function, fx, bd, ce);
+                                return (double)Mo.Substitute("a", a).Substitute("b", b).Substitute("c", c).Substitute("d", d).EvalNumerical();
+                            }
+                        }
+                    }
+                }
+            }
+            MessageBox.Show("Мат. ожидание для первой функции не найдено");
+            return 0; 
+        }
+
+        public double GetS1(double d, double e, List<double> d1, List<double> e1, List<double> k1, List<double> y1)
+        {
+            double interval, S1 = 0;
+            for (int i = 0; i < d1.Count() - 1; i++)
+            {
+                interval = (d1[i + 1] * d + e1[i + 1] * e) / k1[i + 1] - (d1[i] * d + e1[i] * e) / k1[i];
+                S1 += (y1[i] + y1[i + 1]) / 2.0 * interval;
+            }
+            return S1;
+        }
+
+        public double GetP1(double x, double d, double e, List<double> d1, List<double> e1, List<double> k1, List<double> y1)
+        {
+            int left_i = -1;
+            for (int i = 0; i < d1.Count() - 1; i++)
+            {
+                if ((d1[i] * d + e1[i] * e) / k1[i] < x && x < (d1[i + 1] * d + e1[i + 1] * e) / k1[i + 1])
+                {
+                    left_i = i;
+                    continue;
+                }
+                if (x == (d1[i] * d + e1[i] * e) / k1[i])
+                {
+                    return y1[i];
+                }
+                if (x == (d1[i + 1] * d + e1[i + 1] * e) / k1[i + 1])
+                {
+                    return y1[i + 1];
+                }
+            }
+
+
+            if (x < (d1[0] * d + e1[0] * e) / k1[0])
+            {
+                return 0;
+            }
+
+            if (x > (d1[d1.Count() - 1] * d + e1[d1.Count() - 1] * e) / k1[d1.Count() - 1])
+            {
+                return 0;
+            }
+
+            double left_y = y1[left_i], right_y = y1[left_i + 1];
+
+            if (left_y == right_y)
+                return left_y;
+
+            double interval = (d1[left_i + 1] * d + e1[left_i + 1] * e) / k1[left_i + 1] - (d1[left_i] * d + e1[left_i] * e) / k1[left_i];
+
+            double betwen_x_and_left_x = x - (d1[left_i] * d + e1[left_i] * e) / k1[left_i];
+
+            return right_y * betwen_x_and_left_x / interval + left_y * (interval - betwen_x_and_left_x) / interval;
+        }
+
+        public double GetP0(List<double> a0, List<double> x0, List<double> P0, double Mo1, double Mo2)
+        {
+            double Mo0_a = 0, Mo0_x = 0;
+
+            for (int i = 0; i < a0.Count(); i++)
+            {
+                Mo0_a += a0[i] * P0[i];
+                Mo0_x += x0[i] * P0[i];
+            }
+
+            double a, Mo0;
+            Mo0 = 0.87 / (1.0 / Mo1 + 1.0 / Mo2);
+            a = (Mo0 - Mo0_x) / Mo0_a;
+
+
+            double P = 0;
+            for (int i = 0; i < a0.Count(); i++)
+            {
+                P += P0[i];
+                LogOut += Math.Round(P, 1) + ", " + Math.Round((a0[i] * a + x0[i]), 3) + "/\n";
+            }
+
+            return a;
+        }
+
+        public void PrintP1(double S1, double d, double e, List<double> d1, List<double> e1, List<double> k1, List<double> Y1)
+        {
+            LogOut += "\n";
+
+            double P = 0, step = 0.001;
+            for (double i = d; i < e + step; i += step)
+            {
+                if ((i % 1) < step / 2 || (i % 1) > 1 - step / 2)
+                    LogOut += Math.Round(i, 1) + ", " + Math.Round(P, 3) + "/\n";
+                P += GetP1(i, d, e, d1, e1, k1, Y1) * step / S1;
+            }
+
+        }
+
+        public double GetMo1(double S1, double d, double e, List<double> d1, List<double> e1, List<double> k1, List<double> Y1)
+        {
+            double P = 0, step = 0.001, Mo1 = 0, best = 100;
+            for (double i = d; i < e + step; i += step)
+            {
+                P += GetP1(i, d, e, d1, e1, k1, Y1) * step / S1;
+                if (Math.Abs(P - 0.5) < best)
+                {
+                    best = Math.Abs(P - 0.5);
+                    Mo1 = i;
+                }
+            }
+            return Mo1;
+        }
+    }
+}
+
